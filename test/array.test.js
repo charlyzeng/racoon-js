a4c0da2d900df2806db6e2b224b1ce49afb7a2de
chenglezeng <chenglezeng@tencent.com>
2020-08-01 18:45:07 +0800

    test: add unit test about `object`,`array`

---
+++ test/array.test.js
@@ -0,0 +1,110 @@
+import { expect } from 'chai';
+import racoon from '../lib';
+
+describe('`array` function test', () => {
+  it('should restrict the basic type', () => {
+    const schema = racoon.array();
+    expect(schema.validate([])).to.deep.eq([]);
+    const array = [1, 'abc', { a: 1 }, [2, '3']];
+    expect(schema.validate(array)).to.deep.eq(array);
+    expect(() => schema.validate(1)).to.throw('value should be typeof array');
+  });
+
+  it('`min` should restrict the min length of array', () => {
+    const schema = racoon.array().min(3);
+    expect(schema.validate([1, 2, 3])).to.deep.eq([1, 2, 3]);
+    expect(schema.validate(['a', 'b', 'c', 'd'])).to.deep.eq(['a', 'b', 'c', 'd']);
+    expect(() => schema.validate([1, 2])).to.throw('value length should greater than or equal 3');
+
+    const schema2 = racoon.array().min(3, false);
+    expect(() => schema2.validate([1, 2, 3])).to.throw('value length should greater than 3');
+    expect(schema2.validate([1, 2, 3, 4])).to.deep.eq([1, 2, 3, 4]);
+  });
+
+  it('`max` mshould restrict the max length of array', () => {
+    const schema = racoon.array().max(3);
+    expect(schema.validate([1, 2, 3])).to.deep.eq([1, 2, 3]);
+    expect(schema.validate([1, 2])).to.deep.eq([1, 2]);
+    expect(() => schema.validate([1, 2, 3, 4])).to.throw('value length should less than or equal 3');
+
+    const schema2 = racoon.array().max(3, false);
+    expect(() => schema2.validate([1, 2, 3])).to.throw('value length should less than 3');
+    expect(schema2.validate([1, 2])).to.deep.eq([1, 2]);
+  });
+
+  it('`custom` should restrict by user custom function', () => {
+    const schema = racoon.array().custom((array) => {
+      if (array.join('').startsWith('ab')) {
+        return true;
+      }
+      throw new Error('value should starts with "ab"');
+    });
+    expect(schema.validate(['a', 'b'])).to.deep.eq(['a', 'b']);
+    expect(schema.validate(['a', 'b', 'c'])).to.deep.eq(['a', 'b', 'c']);
+    expect(() => schema.validate(['b', 'c'])).to.throw('value should starts with "ab"');
+  });
+
+  it('`required` should restrict the data is required', () => {
+    const schema = racoon.array().required();
+    expect(() => schema.validate()).to.throw('value is required and should not be undefined/null');
+    expect(() => schema.validate(null)).to.throw('value is required and should not be undefined/null');
+
+    const schema2 = racoon.array().required(true);
+    expect(() => schema2.validate()).to.throw('value is required and should not be empty');
+    expect(() => schema2.validate(null)).to.throw('value is required and should not be empty');
+    expect(() => schema2.validate([])).to.throw('value is required and should not be empty');
+  });
+
+  it('`items` should restrict the type of array item', () => {
+    const schema = racoon.array().items(
+      racoon.number().int().min(1).max(6, false)
+    );
+    expect(schema.validate([1, 2, 3, 4, 5])).to.deep.eq([1, 2, 3, 4, 5]);
+    expect(
+      () => schema.validate([1, 2, 3, 4, 5, 6])
+    ).to.throw('"[5]": value should less than 6');
+
+    const schema2 = racoon.array().items(
+      racoon.object({
+        name: racoon.string().min(3).max(5).required(),
+        age: racoon.number().int().min(1).max(199),
+        friends: racoon.array().items(
+          racoon.object({
+            gender: racoon.boolean().required(),
+            age: racoon.number().max(199)
+          })
+        )
+      })
+    );
+    const array = [
+      {
+        name: 'Tom',
+        age: 22,
+        friends: [
+          {
+            age: 2,
+            gender: false
+          }
+        ]
+      },
+      {
+        name: 'Jack',
+        age: 36,
+        friends: [
+          {
+            age: 2,
+            gender: true
+          },
+          {
+            gender: false,
+            age: 199
+          }
+        ]
+      }
+    ];
+    const clone = JSON.parse(JSON.stringify(array));
+    expect(schema2.validate(array)).to.deep.eq(clone);
+    array[1].friends[1].age = 200;
+    expect(() => schema2.validate(array)).to.throw('"[1].friends[1].age": value should less than or equal 199');
+  });
+});
---
+++ test/object.test.js
@@ -0,0 +1,286 @@
+import { expect } from 'chai';
+import racoon from '../lib';
+
+describe('`object` function test', () => {
+  it('should restrict the basic type', () => {
+    const schema = racoon.object();
+    expect(schema.validate()).to.be.undefined;
+    expect(schema.validate(null)).to.be.null;
+    expect(schema.validate({})).to.deep.eq({});
+    expect(() => schema.validate(1)).to.throw('value should be typeof object');
+    expect(
+      () => schema.validate(Promise.resolve())
+    ).to.throw('value should be typeof object');
+    expect(
+      () => schema.validate(() => {})
+    ).to.throw('value should be typeof object');
+  });
+
+  it('`required` should restrict data is required', () => {
+    const schema = racoon.object().required();
+    expect(schema.validate({})).to.deep.eq({});
+    expect(() => schema.validate(null)).to.throw('value is required and should not be undefined/null');
+
+    const schema2 = racoon.object().required(true);
+    expect(() => schema2.validate({})).to.throw('value is required and should not be empty');
+    expect(schema2.validate({ a: 1 })).to.deep.eq({ a: 1 });
+  });
+
+  // it('scene the contains unknown field', () => {
+  //   const schema = racoon.object({
+  //     name: racoon.string().required(),
+  //     age: racoon.number().int().min(1).max(99)
+  //   }).stripUnknown().required();
+  //   const data = {
+  //     name: 'Tom',
+  //     age: null,
+  //     gender: 'this is a unknown field'
+  //   };
+  //   const dataClone = JSON.parse(JSON.stringify(data));
+  //   expect(schema.validate(data)).to.deep.eq({ name: 'Tom', age: null });
+
+  //   const schema2 = racoon.object({
+  //     name: racoon.string().required(),
+  //     age: racoon.number().int().min(1).max(99)
+  //   }).allowUnknown().required();
+  //   expect(schema2.validate(data)).to.deep.eq(dataClone);
+
+  //   const schema3 = racoon.object({
+  //     name: racoon.string().required(),
+  //     age: racoon.number().int().min(1).max(99)
+  //   }).required();
+  //   expect(() => schema3.validate(data)).to.throw('the key `gender` is not allowed');
+
+  //   const schema4 = racoon.object({
+  //     name: racoon.string().required(),
+  //     age: racoon.number().int().min(1).max(99),
+  //     cars: racoon.array().items(
+  //       racoon.object({
+  //         price: racoon.number().min(0).required(),
+  //         brand: racoon.object({
+  //           year: racoon.number().int().required()
+  //         })
+  //       })
+  //     ),
+  //     book: racoon.object({
+  //       title: racoon.string().required()
+  //     })
+  //   }).required();
+  //   expect(
+  //     schema4.validate({
+  //       name: 'Tom',
+  //       age: undefined,
+  //       cars: [
+  //         {
+  //           price: 120000,
+  //           brand: {
+  //             year: 1992
+  //           }
+  //         }
+  //       ],
+  //       book: {
+  //         title: 'War And Peace'
+  //       }
+  //     })
+  //   ).to.deep.eq({
+  //     name: 'Tom',
+  //     age: undefined,
+  //     cars: [
+  //       {
+  //         price: 120000,
+  //         brand: {
+  //           year: 1992
+  //         }
+  //       }
+  //     ],
+  //     book: {
+  //       title: 'War And Peace'
+  //     }
+  //   });
+
+  //   expect(
+  //     () => schema4.validate({
+  //       name: 'Tom',
+  //       age: undefined,
+  //       cars: [
+  //         {
+  //           price: 120000,
+  //           brand: {
+  //             year: 1992,
+  //             name: 'this is a unknown field'
+  //           }
+  //         }
+  //       ],
+  //       book: {
+  //         title: 'War And Peace'
+  //       }
+  //     })
+  //   ).to.throw('"cars[0].brand": the key `name` is not allowed');
+
+  //   expect(
+  //     () => schema4.validate({
+  //       name: 'Tom',
+  //       age: undefined,
+  //       cars: [
+  //         {
+  //           price: 120000,
+  //           brand: {
+  //             year: 1992
+  //           }
+  //         }
+  //       ],
+  //       book: {
+  //         title: 'War And Peace',
+  //         author: 'Lev Nikolayevich Tolstoy'
+  //       }
+  //     })
+  //   ).to.throw('"book": the key `author` is not allowed');
+
+  //   const schema5 = racoon.object({
+  //     name: racoon.string().required(),
+  //     age: racoon.number().int().min(1).max(99),
+  //     cars: racoon.array().items(
+  //       racoon.object({
+  //         price: racoon.number().min(0).required(),
+  //         'prop.with.dot': racoon.object({
+  //           year: racoon.number().int().required()
+  //         })
+  //       })
+  //     ),
+  //     book: racoon.object({
+  //       title: racoon.string().required()
+  //     })
+  //   }).required();
+  //   expect(
+  //     () => schema5.validate({
+  //       name: 'Tom',
+  //       age: undefined,
+  //       cars: [
+  //         {
+  //           price: 120000,
+  //           'prop.with.dot': {
+  //             year: 1992,
+  //             name: 'this is a unknown field'
+  //           }
+  //         }
+  //       ],
+  //       book: {
+  //         title: 'War And Peace'
+  //       }
+  //     })
+  //   ).to.throw('"cars[0][\'prop.with.dot\']": the key `name` is not allowed');
+  // });
+
+  it('complex scene 1', () => {
+    const schema = racoon.object({
+      name: racoon.string().min(3).max(9).required(),
+      age: racoon.number().int().min(0).max(199),
+      isDanger: racoon.boolean().required()
+    }).required(true);
+    expect(
+      schema.validate({
+        name: 'Tom',
+        age: 22,
+        isDanger: false
+      })
+    ).to.deep.eq({
+      name: 'Tom',
+      age: 22,
+      isDanger: false
+    });
+    expect(() => schema.validate({})).to.throw('value is required and should not be empty');
+    expect(() => schema.validate()).to.throw('value is required and should not be empty');
+    expect(
+      () => schema.validate({
+        name: 'Tom',
+        age: 200,
+        isDanger: true
+      })
+    ).to.throw('"age": value should less than or equal 199');
+  });
+
+  it('complex scene 2', () => {
+    const schema = racoon.object({
+      name: racoon.string().min(3).max(9).required(),
+      age: racoon.number().int().min(0).max(199),
+      isDanger: racoon.boolean().required(),
+      friend: racoon.object({
+        name: racoon.string().required(),
+        age: racoon.number().int()
+      }).required()
+    }).required(true);
+    expect(
+      schema.validate({
+        name: 'Tom',
+        age: 22,
+        isDanger: false,
+        friend: {
+          name: 'Jack',
+          age: 20
+        }
+      })
+    ).to.deep.eq({
+      name: 'Tom',
+      age: 22,
+      isDanger: false,
+      friend: {
+        name: 'Jack',
+        age: 20
+      }
+    });
+    expect(
+      () => schema.validate({
+        name: 'Tom',
+        age: 22,
+        isDanger: true,
+        friend: {
+          name: 'Jack',
+          age: 22.3
+        }
+      })
+    ).to.throw('"friend.age": value should be an int');
+  });
+
+  it('complex scene 3', () => {
+    const schema = racoon.object({
+      prop1: racoon.array().items(
+        racoon.object({
+          prop2: racoon.object({
+            prop3: racoon.array().items(
+              racoon.array().items(
+                racoon.object({
+                  prop4: racoon
+                    .number()
+                    .int()
+                    .min(1)
+                    .max(3, false)
+                    .required()
+                })
+              )
+            )
+          })
+        })
+      )
+    }).required(true);
+    const data = {
+      prop1: [
+        {
+          prop2: {
+            prop3: [
+              [],
+              [
+                {
+                  prop4: 3
+                }
+              ]
+            ]
+          }
+        }
+      ]
+    };
+    expect(() => schema.validate(data)).to.throw('"prop1[0].prop2.prop3[1][0].prop4": value should less than 3');
+    data.prop1[0].prop2.prop3[1][0].prop4 = 2;
+    const clone = JSON.parse(JSON.stringify(data));
+    expect(schema.validate(data)).to.deep.eq(clone);
+  });
+});
