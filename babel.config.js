e206ed0faa7d69eefd002ae93896d048ac9f4269
chenglezeng <chenglezeng@tencent.com>
2020-08-01 18:23:30 +0800

    chore(eslint): add eslint config

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env'
+  ],
+  plugins: [
+    [
+      '@babel/plugin-proposal-class-properties', { loose: true }
+    ],
+    '@babel/plugin-proposal-object-rest-spread'
+  ]
+};
